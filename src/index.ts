0350d3ff8e94a0c3b102602c524b971b5f6edf68
elvinos <elvinos33@duck.com>
2023-09-14 20:36:18 +0200

    migration and post

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { PrismaClient } from '@prisma/client'
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new PrismaClient()
+
+const app = new Elysia()
+    .use(swagger())
+    .post(
+        '/sign-up',
+        async ({ body }) => db.user.create({
+            data: body
+        }),
+        {
+            body: t.Object({
+                username: t.String(),
+                password: t.String({
+                    minLength: 8
+                })
+            })
+        }
+
+    )
+    .listen(8000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
